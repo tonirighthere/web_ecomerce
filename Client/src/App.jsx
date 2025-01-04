a34efc20b73c9da883ea056422c63c5efc6ac848
Dinesh R <144953120+16Dinesh@users.noreply.github.com>
2025-01-04 12:00:57 +0530

    Basics

---
+++ Client/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div>
+      <h2 className='text-4xl font-bold'>Test Tailwind</h2>
+     </div>
+    </>
+  )
+}
+
+export default App
---
+++ readme.md
@@ -0,0 +1,10 @@
+# MERN E-commerce Application (Local)
+
+## Technologies Used
+
+- **Frontend**: React (Vite), Tailwind CSS, Redux Toolkit
+- **Backend**: Node.js, Express, Cloudinary
+- **Database**: MongoDB
+- **Payment**: PayPal
+
+# Building Currently
